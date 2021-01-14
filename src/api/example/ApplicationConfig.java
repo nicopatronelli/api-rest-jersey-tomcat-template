2430f314d7e845fa41af5cbf60ee67e10d8d30fd
Nicolas Patronelli <nicolas.patronelli@neoris.com>
2021-01-14 15:16:57 -0300

    ResourceConfig extends javax.ws.rsApplication class

--- src/api/example/ApplicationConfig.java
+++ src/api/example/ApplicationConfig.java
@@ -2,6 +2,7 @@ package api.example;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+// ResourceConfig extiende de javax.ws.rs.Application
 public class ApplicationConfig extends ResourceConfig {
 	public ApplicationConfig() {
 		this.register(new ApplicationBinder());
